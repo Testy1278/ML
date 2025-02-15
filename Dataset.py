1e47e8b22cb98dff9a695c9905b69359cd3e5c92
Testy1278 <losiev.artem@gmail.com>
2025-02-15 19:19:08 +0300

    upgrade performance

--- Dataset.py
+++ Dataset.py
@@ -1,6 +1,6 @@
 
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import LabelEncoder
+from category_encoders import TargetEncoder
 import pandas as pd
 
 
@@ -9,18 +9,21 @@ df = pd.read_csv('laptop_data.csv') # считываем таблицу
 df = df.drop(["Unnamed: 0"], axis= 1) # удаляем ненужный столбец
 
 
-encoder = OneHotEncoder(sparse_output=False, drop='first')
+encoder = OneHotEncoder(sparse_output=False, drop='first', handle_unknown='ignore')
 
-encoded_feathur = encoder.fit_transform(df[['Company', 'Cpu', 'Ram', 'Memory', 'Gpu', 'OpSys']])
-encoded_df = pd.DataFrame(encoded_feathur, columns= encoder.get_feature_names_out(['Company', 'Cpu', 'Ram', 'Memory', 'Gpu', 'OpSys']))
+encoded_features = encoder.fit_transform(df[['Company', 'Cpu', 'Ram', 'Memory', 'OpSys']])
+encoded_df = pd.DataFrame(encoded_features, columns= encoder.get_feature_names_out(['Company', 'Cpu', 'Ram', 'Memory', 'OpSys']))
 
-df = df.drop(['Company', 'Cpu', 'Ram', 'Memory', 'Gpu', 'OpSys'], axis= 1)
+df = df.drop(['Company', 'Cpu', 'Ram', 'Memory', 'OpSys'], axis= 1)
 df = pd.concat([df, encoded_df], axis= 1)
 
-label_encoder = LabelEncoder()
-df['Weight'] = label_encoder.fit_transform(df['Weight'])
-df['TypeName'] = label_encoder.fit_transform(df['TypeName'])
-df[ 'ScreenResolution'] = label_encoder.fit_transform(df[ 'ScreenResolution'])
+
+
+target_encoder = TargetEncoder()
+df['ScreenResolution'] = target_encoder.fit_transform(df['ScreenResolution'], df['Price'])
+df['Weight'] = target_encoder.fit_transform(df['Weight'], df['Price'])
+df['TypeName'] = target_encoder.fit_transform(df['TypeName'], df['Price'])
+df['Gpu'] = target_encoder.fit_transform(df['Gpu'], df['Price'])
 
 
 
--- ML.py
+++ ML.py
@@ -3,13 +3,14 @@ from Dataset import df
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import joblib
 
 
 
 X = df.drop('Price', axis= 1)
 Y = df['Price']
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size= 0.3, random_state= 42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size= 0.1, random_state= 42)
 
 model = LinearRegression()
 model.fit(X_train, Y_train)
@@ -17,3 +18,5 @@ model.fit(X_train, Y_train)
 y_pred = model.predict(X_test)
 print("Mean squared error:", mean_squared_error(Y_test, y_pred))
 print(f"R^2 Score: {r2_score(Y_test, y_pred)}")
+
+joblib.dump(model, "First_ML.pkl")
