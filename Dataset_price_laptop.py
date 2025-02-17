dcf455675f240b84f8b03046c59dc56fc93e5707
Testy1278 <losiev.artem@gmail.com>
2025-02-17 18:08:38 +0300

    "bip bip bip"

--- Dataset_price_laptop.py
+++ Dataset_price_laptop.py
@@ -8,7 +8,6 @@ df = pd.read_csv('laptop_data.csv') # считываем таблицу
 
 df = df.drop(["Unnamed: 0"], axis= 1) # удаляем ненужный столбец
 
-
 encoder = OneHotEncoder(sparse_output=False, drop='first', handle_unknown='ignore') # создаем объект кодироки OneHot
 
 encoded_features = encoder.fit_transform(df[['Company', 'Cpu', 'Ram', 'Memory', 'OpSys']]) # кодируем с помощью ее
@@ -17,8 +16,6 @@ encoded_df = pd.DataFrame(encoded_features,
 
 df = df.drop(['Company', 'Cpu', 'Ram', 'Memory', 'OpSys'], axis= 1) # удаляем столбцы старого формата
 df = pd.concat([df, encoded_df], axis= 1) # соединяем старые данные с новыми
-
-
 
 target_encoder = TargetEncoder() 
 df['ScreenResolution'] = target_encoder.fit_transform(df['ScreenResolution'], df['Price'])
